d3d9c6f27c86b65888de62b47780c1da60f9b766
Tim <tim.maastricht@gmail.com>
2026-03-16 12:22:40 +1000

    feat: add search by title, search by ratings functionality

--- frontend/src/components/movies-list.js
+++ frontend/src/components/movies-list.js
@@ -8,22 +8,46 @@ import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
 
-const MoviesList = (props) => { // this could be more explicit
+const MoviesList = (props) => {
+  // this could be more explicit
   const [movies, setMovies] = useState([]);
   const [searchTitle, setSearchTitle] = useState("");
   const [searchRating, setSearchRating] = useState("");
   const [ratings, setRatings] = useState(["All Ratings"]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [entriesPerPage, setEntriesPerPage] = useState(0);
+  const [currentSearchMode, setCurrentSearchMode] = useState(" ");
 
   useEffect(() => {
     retrieveMovies();
     retrieveRatings();
   }, []);
 
+  useEffect(() => {
+    setCurrentPage(0)
+  },[currentSearchMode])
+
+  useEffect(() => {
+    // retrieveMovies();
+    retrieveNextPage()
+  }, [currentPage]);
+
+  const retrieveNextPage =() => {
+    if(currentSearchMode === "findByTitle")
+      findByTitle()
+    else if(currentSearchMode === "findByRating")
+      findByRating()
+    else
+      retrieveMovies()
+  }
+
   const retrieveMovies = () => {
-    MovieDataService.getAll()
+    MovieDataService.getAll(currentPage)
       .then((response) => {
         console.log(response.data);
         setMovies(response.data.movies);
+        setCurrentPage(response.data.page);
+        setEntriesPerPage(response.data.entries_per_page);
       })
       .catch((e) => {
         console.log(e);
@@ -54,7 +78,7 @@ const MoviesList = (props) => { // this could be more explicit
 
   // find provides the search query value entered by the user
   // find calls the backend api
-  const find = (query, by) => {
+  const find = (query, by, currentPage) => {
     MovieDataService.find(query, by)
       .then((response) => {
         console.log(response.data);
@@ -80,9 +104,9 @@ const MoviesList = (props) => { // this could be more explicit
       find(searchRating, "rated");
     }
   };
-// TODO: move custom styling to .css, remove unused props 
+  // TODO: move custom styling to .css, remove unused props
   return (
-    <div className="App" style={{ paddingTop: "6%"}}>
+    <div className="App" style={{ paddingTop: "6%" }}>
       <Container>
         <Form>
           <Row>
@@ -130,6 +154,16 @@ const MoviesList = (props) => { // this could be more explicit
             );
           })}
         </Row>
+        <br />
+        Showing page: {currentPage}.
+        <Button
+          variant="link"
+          onClick={() => {
+            setCurrentPage(currentPage + 1);
+          }}
+        >
+          Get next {entriesPerPage} results
+        </Button>
       </Container>
     </div>
   );
