521b3a7bb224b71b7ce7ffbf18634ffb042313f6
Tim <tim.maastricht@gmail.com>
2026-03-16 12:47:03 +1000

    feat: FE api calls to hosted backend

--- frontend/src/services/movies.js
+++ frontend/src/services/movies.js
@@ -5,40 +5,40 @@ class MovieDataService {
 
   // this returns all the movies for a particular page
   getAll(page = 0){
-    return axios.get(`http://localhost:3000/api/v1/movies?page=${page}`)
+    return axios.get(`https://movie-reviews-9bew.onrender.com/api/v1/movies?page=${page}`)
   }
 
   // get specific movie with supplied id
   get(id) {
-    return axios.get(`http://localhost:3000/api/v1/movies/id/${id}`)
+    return axios.get(`https://movie-reviews-9bew.onrender.com/api/v1/movies/id/${id}`)
   }
 
   // this connects to the same endpoint as getAll except it has query which consists of the
   // user-entered search title, ratings and page number
   find(query, by = "title", page = 0) {
     return axios.get(
-      `http://localhost:3000/api/v1/movies?${by}=${query}&page=${page}`
+      `https://movie-reviews-9bew.onrender.com/api/v1/movies?${by}=${query}&page=${page}`
     )
   }
 
   // the next four methods are for CRUD ops
   createReview(data) {
-    return axios.post("http://localhost:3000/api/v1/movies/review", data)
+    return axios.post("https://movie-reviews-9bew.onrender.com/api/v1/movies/review", data)
   }
   
   updateReview(data) {
-    return axios.put("http://localhost:3000/api/v1/movies/review", data)
+    return axios.put("https://movie-reviews-9bew.onrender.com/api/v1/movies/review", data)
   }
 
   deleteReview(id, userId) {
     return axios.delete(
-      "http://localhost:3000/api/v1/movies/review",
+      "https://movie-reviews-9bew.onrender.com/api/v1/movies/review",
       {data:{review_id: id, user_id: userId}}
     )
   }
 
   getRatings(){
-    return axios.get("http://localhost:3000/api/v1/movies/ratings")
+    return axios.get("https://movie-reviews-9bew.onrender.com/api/v1/movies/ratings")
   }
 }
 
