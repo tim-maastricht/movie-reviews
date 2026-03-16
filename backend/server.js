366b8b78557cfcb5ba80682800abecce490f6004
Tim <tim.maastricht@gmail.com>
2026-03-16 12:50:14 +1000

    feat: add cors

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,28 @@
-import express from 'express'
-import cors from 'cors'
-import movies from './api/movies.route.js'
+import express from "express";
+import cors from "cors";
+import movies from "./api/movies.route.js";
 
-const app = express()
+const app = express();
 
-app.use(cors( // keep for workaround
-//   {
-//   origin: '*',
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   allowedHeaders: ['Content-Type']
-// }
-))
-app.use(express.json())
+app.use(
+  cors(
+    {
+      origin: "*",
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    },
+  ),
+);
 
-app.use("/api/v1/movies", movies) // TODO: refactor
-app.use('/./', (request, response) => { // using regex because wildcard * was invalid
-  response.status(404).json({error: "not found"})
-})
+// enable preflight response for all routes
+app.options("*", cors());
+
+app.use(express.json());
+
+app.use("/api/v1/movies", movies); // TODO: refactor
+app.use("/./", (request, response) => {
+  // using regex because wildcard * was invalid
+  response.status(404).json({ error: "not found" });
+});
 // TODO: add more custom response status codes
-export default app
+export default app;
